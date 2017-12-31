73a104643a895cdceb8ad12ab6d00b57157521a0
Lorenzo <lorenzo535@gmail.com>
2017-12-31 13:13:47 +0100

    tested OK in car

--- GPS_boat_speed.ino
+++ GPS_boat_speed.ino
@@ -4,20 +4,25 @@
 #include <Streaming.h>
 
 #define GPS_TX 4
-#define GPS_RX 12 //unused
+#define GPS_RX 11 //unused
 
 #define SQUAREWAVE_OUT 2
 #define MILESCOUNTER_OUT 3
 #define SEC_TO_HOURS 0.000277778f
 #define KNOTS_TO_HZ 17
 #define OCR1A_1HZ 15624     //value of the timer counter for a 1Hz interrupt call
-#define GPSECHO false
+#define GPSECHO 0
 #define DISTANCE_INTEGRAL_STEP_S 3
 #define S_TO_MS 1000.0f
 #define MS_TO_S 0.001f
-SoftwareSerial mySerial(GPS_TX, GPS_RX);
 
+
+#define LED_GPS_COMM 13
+#define LED_GPS_VALID 12
+
+SoftwareSerial mySerial(GPS_TX, GPS_RX);
 Adafruit_GPS GPS(&mySerial);
+
 //char readbuffer [64] = {0};
 //char c;
 
@@ -36,6 +41,7 @@ uint32_t odometer_timer = millis();
 uint32_t odo_reset_time = millis();
 uint32_t time_delta = 0;
 uint32_t millis_;
+uint32_t millissincelast = timer;
 boolean manual_commands;
 float miles_travelled;
 float old_miles_travelled;
@@ -45,28 +51,30 @@ float delta;
 int steps, i;
 int total_steps = 0;
 float old_speed;
-boolean flipflop; 
+boolean flipflop;
 boolean manual_high_speed = false;
 
-//#define GPSECHO 1
-void setup()  
+
+void setup()
 {
-    
+
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   // also spit it out
   Serial.begin(9600);
-  Serial.println("Adafruit GPS library basic test!");
+  Serial.println("GPS Boat speed display");
 
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(4800);
   //mySerial.begin (4800);
-  
+
   useInterrupt(true);
 
   delay(1000);
 
   pinMode(SQUAREWAVE_OUT, OUTPUT);
   pinMode(MILESCOUNTER_OUT, OUTPUT);
+  pinMode(LED_GPS_COMM, OUTPUT);
+  pinMode (LED_GPS_VALID, OUTPUT);
   miles_travelled = 0.0;
   old_miles_travelled = 0.0;
   old_speed = 0.0;
@@ -76,10 +84,10 @@ void setup()
 
   digitalWrite(MILESCOUNTER_OUT, true);
 
-//
-cli();//stop interrupts
+  //
+  cli();//stop interrupts
 
-//set timer1 interrupt at 1Hz
+  //set timer1 interrupt at 1Hz
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
@@ -88,12 +96,12 @@ cli();//stop interrupts
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
-  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  TCCR1B |= (1 << CS12) | (1 << CS10);
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
-sei();//allow interrupts
-//
+  sei();//allow interrupts
+  //
 
 
 }
@@ -102,31 +110,31 @@ sei();//allow interrupts
 void SetCompareMatchRegisterForHertz(float hzsetpoint)
 {
 
-//compare match register = [ 16,000,000Hz/ (prescaler * desired interrupt frequency) ] - 1
-// prescaler is 1024
+  //compare match register = [ 16,000,000Hz/ (prescaler * desired interrupt frequency) ] - 1
+  // prescaler is 1024
 
-//vcalibration tweak
-if (hzsetpoint < 40)
-  hzsetpoint = hzsetpoint * 0.9;
+  //vcalibration tweak
+  if (hzsetpoint < 40)
+    hzsetpoint = hzsetpoint * 0.9;
 
-cli();
-//OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
-TCNT1  = 0;
+  cli();
+  //OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  TCNT1  = 0;
 
-if (hzsetpoint == -1)
-  OCR1A = 65535;
-else
-  OCR1A = OCR1A_1HZ / hzsetpoint;
+  if (hzsetpoint == -1)
+    OCR1A = 65535;
+  else
+    OCR1A = OCR1A_1HZ / hzsetpoint;
 
-sei();
+  sei();
 
 }
 
 
 ISR(TIMER1_COMPA_vect)
 {
-  flipflop = !flipflop;      
-  digitalWrite (SQUAREWAVE_OUT,flipflop);
+  flipflop = !flipflop;
+  digitalWrite (SQUAREWAVE_OUT, flipflop);
   //Serial <<".";
 }
 
@@ -137,9 +145,9 @@ SIGNAL(TIMER0_COMPA_vect) {
   // if you want to debug, this is a good time to do it!
 #ifdef UDR0
   if (GPSECHO)
-    if (c) UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
+    if (c) UDR0 = c;
+  // writing direct to UDR0 is much much faster than Serial.print
+  // but only one character can be written at a time.
 #endif
 }
 
@@ -160,58 +168,85 @@ void useInterrupt(boolean v) {
 
 void loop()                     // run over and over again
 {
+/*  
+    //digitalWrite (LED_GPS_VALID, false);
+    digitalWrite (SQUAREWAVE_OUT, true);
+    Serial << "ping \n";
+    delay (100);
+    digitalWrite (SQUAREWAVE_OUT, false);
+    Serial << "pong \n";
+    delay (100);
 
-  
-  // if a sentence is received, we can check the checksum, parse it...
-   //char c = GPS.read();
-   //if (c) Serial.print(c);
+    return;
 
-   //if (mySerial.available()>0)
-   //{
-   ///c = mySerial.read();
-  // if (c) Serial.print(c);
-   //}
-   
- 
-  if ((GPS.newNMEAreceived())&&(!manual_commands)) {
-  
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false    
-     return;  // we can fail to parse a sentence in which case we should just wait for another      
-     else
-     {
-      if (GPS.fix)      
+    digitalWrite (LED_GPS_VALID, true);
+
+    delay (1000);
+    digitalWrite (LED_GPS_VALID, false);
+    delay (1000);
+  */
+
+if (!manual_commands)
+{
+  if (GPS.newNMEAreceived())
+  {
+
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+    else
+    {
+      millissincelast = millis();
+      //Serial << "decode ok\n";
+      if (GPS.fix)
       {
         GPS_speed = GPS.speed;
-          
-        if (GPS_speed >= 0.1)
+        digitalWrite (LED_GPS_VALID, true);
+        digitalWrite (LED_GPS_COMM, false);
+
+        if (GPS_speed >= 0.5)
         {
-        
           if (GPS.seconds != old_GPS_seconds)
           {
             Serial << "Fix valid ; speed is " << GPS_speed << "\n";
             old_GPS_seconds = GPS.seconds;
-            SetCompareMatchRegisterForHertz( GPS_speed * KNOTS_TO_HZ);        
+            SetCompareMatchRegisterForHertz( GPS_speed * KNOTS_TO_HZ);
             ComputeDistanceTravelled(GPS_speed);
           }
         }
         else
-           SetCompareMatchRegisterForHertz( -1);
+          SetCompareMatchRegisterForHertz( -1);
       }
-    else
-      SetCompareMatchRegisterForHertz( -1);
-     }
-             
+      else
+      {
+        SetCompareMatchRegisterForHertz( -1);
+        digitalWrite (LED_GPS_VALID, true);
+        digitalWrite (LED_GPS_COMM, true);
+        //Serial << "signal not valid\n";
+      }
+    }
+
+  }// end if received
+  else
+  {
+    if (millis() - millissincelast > 3000)
+    {
+      digitalWrite (LED_GPS_COMM, true);
+      digitalWrite (LED_GPS_VALID, false);
+      //Serial << "no signal\n";
+    }
   }
+}
 
-   if (manual_commands)
-   ComputeDistanceTravelled(manual_speed);                     
 
-       
+  if (manual_commands)
+    ComputeDistanceTravelled(manual_speed);
+
+
   millis_ = millis();
   // if millis() or timer wraps around, we'll just reset it
   if (timer > millis_)  timer = millis_;
   if (odometer_timer > millis_)  odometer_timer = millis_;
-  
+
   if (display_GPS_data)
     PrintStuff();
 
@@ -220,99 +255,99 @@ void loop()                     // run over and over again
 
 void ReadKeyboardCmds()
 {
-   if (Serial.available() > 0) 
-   {  
-      char rx_byte = Serial.read();       // get the character
-    
-      // check if test command
-      if ((rx_byte == 't') || (rx_byte == 'T')) 
+  if (Serial.available() > 0)
+  {
+    char rx_byte = Serial.read();       // get the character
+
+    // check if test command
+    if ((rx_byte == 't') || (rx_byte == 'T'))
+    {
+      StepOdometerIncrement();
+
+    }
+
+    // check if test command
+    if ((rx_byte == 'g') || (rx_byte == 'G'))
+    {
+      display_GPS_data = !display_GPS_data;
+
+    }
+
+    if ((rx_byte == 'o') || (rx_byte == 'O'))
+    {
+      ResetOdometer();
+    }
+
+    if ((rx_byte == 'h') || (rx_byte == 'H'))
+    {
+      manual_high_speed = ! manual_high_speed;
+    }
+
+    // check if manual command
+    if ((rx_byte == 'x') || (rx_byte == 'X'))
+    {
+      manual_commands = !manual_commands;
+      Serial << "switched to " << (manual_commands ? "manual" : "programmed") << "controls \n";
+
+    }
+    if (manual_commands)
+    {
+      if ((rx_byte >= '1') && (rx_byte <= '9'))
       {
-        StepOdometerIncrement();
-        
+        manual_speed = int (rx_byte) - 48;
+        if (manual_high_speed) manual_speed += 9;
+        Serial << "//New speed " << manual_speed << " knots\\ \n";
+        SetCompareMatchRegisterForHertz( manual_speed * KNOTS_TO_HZ);
       }
 
-      // check if test command
-      if ((rx_byte == 'g') || (rx_byte == 'G')) 
-      {
-        display_GPS_data = !display_GPS_data;
-        
-      }
+    }
 
-      if ((rx_byte == 'o') || (rx_byte == 'O')) 
-      {
-        ResetOdometer();        
-      }
-
-      if ((rx_byte == 'h') || (rx_byte == 'H')) 
-      {
-       manual_high_speed = ! manual_high_speed;
-      }
-      
-      // check if manual command
-      if ((rx_byte == 'x') || (rx_byte == 'X')) 
-      {
-        manual_commands =!manual_commands;
-        Serial << "switched to " << (manual_commands ? "manual" : "programmed") << "controls \n";
-        
-      }
-      if (manual_commands)
-        {
-          if ((rx_byte >= '1') && (rx_byte <= '9'))
-          {
-          manual_speed = int (rx_byte) - 48;
-          if (manual_high_speed) manual_speed += 9;
-          Serial << "//New speed " << manual_speed << " knots\\ \n";          
-          SetCompareMatchRegisterForHertz( manual_speed * KNOTS_TO_HZ);
-          }
-          
-        }
-  
   }
 }
 
 
 void ComputeDistanceTravelled( float speed)
 {
-//Compute distance travelled
+  //Compute distance travelled
   time_delta = millis() - odometer_timer ;
-    if (time_delta > DISTANCE_INTEGRAL_STEP_S * S_TO_MS) 
-    { 
-      odometer_timer = millis(); // reset the timer
-      
-      delta = (speed + old_speed) * time_delta * MS_TO_S * SEC_TO_HOURS /2.0f;  
-      miles_travelled = miles_travelled  + delta;
-      delta_travelled = miles_travelled - old_miles_travelled;
-      
-      old_speed= speed;
+  if (time_delta > DISTANCE_INTEGRAL_STEP_S * S_TO_MS)
+  {
+    odometer_timer = millis(); // reset the timer
 
-       
-     //Serial << "time delta " <<_FLOAT (time_delta, 4) << "=== delta distance " << _FLOAT (delta, 4) << "\n";
-     
-     if ((delta_travelled) > 0.01)
-     {
-       steps = (int)(delta_travelled / 0.01);
-       //Serial << "delta: "<< _FLOAT (delta_travelled, 4) << "  steps: "<< steps << "\n";
-       for (i = 0; i < steps; i++)
-       {
+    delta = (speed + old_speed) * time_delta * MS_TO_S * SEC_TO_HOURS / 2.0f;
+    miles_travelled = miles_travelled  + delta;
+    delta_travelled = miles_travelled - old_miles_travelled;
+
+    old_speed = speed;
+
+
+    //Serial << "time delta " <<_FLOAT (time_delta, 4) << "=== delta distance " << _FLOAT (delta, 4) << "\n";
+
+    if ((delta_travelled) > 0.01)
+    {
+      steps = (int)(delta_travelled / 0.01);
+      //Serial << "delta: "<< _FLOAT (delta_travelled, 4) << "  steps: "<< steps << "\n";
+      for (i = 0; i < steps; i++)
+      {
         StepOdometerIncrement();
-        total_steps = total_steps +1;
-       }
+        total_steps = total_steps + 1;
+      }
 
-       old_miles_travelled = miles_travelled - (delta_travelled - steps * 0.01);
-       Serial << "delta_travelled " <<_FLOAT (delta_travelled, 4);
-       Serial << "miles travelled  " << _FLOAT (miles_travelled, 4) << " old_miles_travelled" << _FLOAT (old_miles_travelled,4) << "\n";       
-       Serial << "miles are now " << miles_travelled << " speed is " << speed << " total steps sent: "<< total_steps <<"\n";
-     }
-     
-     
+      old_miles_travelled = miles_travelled - (delta_travelled - steps * 0.01);
+      Serial << "delta_travelled " << _FLOAT (delta_travelled, 4);
+      Serial << "miles travelled  " << _FLOAT (miles_travelled, 4) << " old_miles_travelled" << _FLOAT (old_miles_travelled, 4) << "\n";
+      Serial << "miles are now " << miles_travelled << " speed is " << speed << " total steps sent: " << total_steps << "\n";
     }
+
+
+  }
 }
 
 void ResetOdometer()
 {
   Serial << " ### RESET ### \n";
-  Serial << " Odometer was running since " << (millis () - odo_reset_time) * MS_TO_S<< " seconds, distance travelled:" << miles_travelled <<"\n" ;
-  
+  Serial << " Odometer was running since " << (millis () - odo_reset_time) * MS_TO_S << " seconds, distance travelled:" << miles_travelled << "\n" ;
+
   miles_travelled = 0.0;
   delta_travelled = 0.0;
   old_speed = 0.0;
@@ -323,12 +358,12 @@ void ResetOdometer()
 
 void StepOdometerIncrement()
 {
-        digitalWrite(MILESCOUNTER_OUT, true);
-       delay (200);
-       digitalWrite(MILESCOUNTER_OUT, false);
-       delay (200);
-       Serial << "step \n";
- 
+  digitalWrite(MILESCOUNTER_OUT, true);
+  delay (200);
+  digitalWrite(MILESCOUNTER_OUT, false);
+  delay (200);
+  Serial << "step \n";
+
 
 }
 
@@ -336,9 +371,9 @@ void PrintStuff()
 {
 
   // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) { 
+  if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
-   
+
     Serial.print("\nTime: ");
     Serial.print(GPS.hour, DEC); Serial.print(':');
     Serial.print(GPS.minute, DEC); Serial.print(':');
@@ -349,22 +384,22 @@ void PrintStuff()
     Serial.print(GPS.month, DEC); Serial.print("/20");
     Serial.println(GPS.year, DEC);
     Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
     if (GPS.fix) {
       Serial.print("Location: ");
       Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", "); 
+      Serial.print(", ");
       Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
       Serial.print("Location (in degrees, works with Google Maps): ");
       Serial.print(GPS.latitudeDegrees, 4);
-      Serial.print(", "); 
+      Serial.print(", ");
       Serial.println(GPS.longitudeDegrees, 4);
-      
+
       Serial.print("Speed (knots): "); Serial.println(GPS.speed);
       Serial.print("Angle: "); Serial.println(GPS.angle);
       Serial.print("Altitude: "); Serial.println(GPS.altitude);
       Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
     }
   }
-  
+
 }
